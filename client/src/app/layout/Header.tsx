7a3461f01e4ea889c8141ed8bf56cd55488a92d3
joy016 <jhoylejas@gmail.com>
2022-12-11 03:00:39 +0800

    End of Section 3 lesson

---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,18 @@
+import { AppBar, Toolbar, IconButton, Typography, Button, Switch } from "@mui/material";
+interface Props{
+  darkMode: boolean;
+  handleThemeChange: () => void;
+}
+
+export default function Header({darkMode, handleThemeChange}: Props){
+    return(
+        <AppBar position="static" sx={{mb: 4}}>
+        <Toolbar>
+          <Typography variant="h6">
+            Yugee and Boki Store
+          </Typography>
+          <Switch checked={darkMode} onChange={handleThemeChange}/>
+        </Toolbar>
+      </AppBar>
+    )
+}
